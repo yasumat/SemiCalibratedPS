a0a344c217feffc356ef8a47c77829582db31348
yasumat <yasumat@ist.osaka-u.ac.jp>
2019-01-08 18:59:13 +0900

    First commit

---
+++ scps.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Semi-Calibrated Photometric Stereo in Python
+"""
+__author__ = "Yasuyuki Matsushita <yasumat@ist.osaka-u.ac.jp>"
+__version__ = "0.1.0"
+__date__ = "08 Jan 2019"
+
